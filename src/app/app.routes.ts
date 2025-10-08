9edefe2543c1c7a8b985b683e5d0ddbfe33b0141
JuaniCabral <juanicabral06@gmail.com>
2025-10-08 11:36:39 -0300

    approute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,34 +5,39 @@ import { ContactDetailsPage } from './pages/contact-details-page/contact-details
 import { LoggedLayout } from './layouts/logged-layout/logged-layout';
 import { GroupsPage } from './pages/groups/groups';
 import { RegisterPage } from './pages/register-page/register-page';
-import { onlyLoggedUserGuard } from './guards/only-logged-user-guard';
 import { onlyPublicUserGuard } from './guards/only-public-user-guard';
+import { onlyLoggedUserGuard } from './guards/only-logged-user-guard';
 import { NewEditContact } from './pages/new-edit-contact/new-edit-contact';
 
 export const routes: Routes = [
   {
     path: "login",
-    component: LoginPage
-
+    component: LoginPage,
+    canActivate: [onlyPublicUserGuard]
   },
   {
     path: "register",
-    component: RegisterPage
+    component: RegisterPage,
+    canActivate: [onlyPublicUserGuard]
   },
   {
     path: "",
     component: LoggedLayout,
+    canActivateChild: [onlyLoggedUserGuard],
     children: [
-      {
-        path:"",
-        component:NewEditContact,
-      },
       {
         path: "",
         component: ContactsPage
+      },{
+        path: "contacts/new",
+        component: NewEditContact
       },
       {
-        path: "contacts/:id/edit",
+        path: "contacts/:idContacto",
+        component: ContactDetailsPage
+      },
+      {
+        path: "contacts/:idContacto/edit",
         component: NewEditContact
       },
       {
