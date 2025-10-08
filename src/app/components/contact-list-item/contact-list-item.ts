0850a18d3cb3202bb698247ef46f79eb79e3f578
JuaniCabral <juanicabral06@gmail.com>
2025-10-08 09:12:04 -0300

    alertas!

--- src/app/components/contact-list-item/contact-list-item.ts
+++ src/app/components/contact-list-item/contact-list-item.ts
@@ -2,7 +2,7 @@ import { Component, inject, input } from '@angular/core';
 import { Contact } from '../../interfaces/contact';
 import { ContactsService } from '../../services/contacts-service';
 import { RouterModule } from '@angular/router';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-contact-list-item',
   imports: [RouterModule],
@@ -13,4 +13,22 @@ export class ContactListItem {
   contact = input.required<Contact>()
   aleatorio = Math.random()
   contactsService = inject(ContactsService)
+
+  openDeleteModal(){
+    Swal.fire({
+      title: "¿desea cerrar sesión?",
+      showDenyButton: true,
+      showCancelButton: true,
+      showConfirmButton: false,
+      confirmButtonText: "Cancelar",
+      denyButtonText: `Cerrar sesión `
+     }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Saved!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+     });
+  }
 }
--- src/app/layouts/logged-layout/logged-layout.ts
+++ src/app/layouts/logged-layout/logged-layout.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-logged-layout',
   imports: [RouterOutlet, RouterLink],
@@ -9,4 +10,24 @@ import { AuthService } from '../../services/auth-service';
 })
 export class LoggedLayout {
  authService = inject(AuthService);
+
+ openloggoutModal(){
+    Swal.fire({
+      title: "¿desea cerrar sesión?",
+      showDenyButton: true,
+      showCancelButton: true,
+      showConfirmButton: false,
+      confirmButtonText: "Cancelar",
+      denyButtonText: `Cerrar sesión `
+     }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Saved!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+     });
+ }
 }
+
+
